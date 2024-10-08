6d89bc470ea84677dbba588b349cbf00c4d19472
DoloresM <DoloresM@users.noreply.github.com>
2024-10-08 14:34:04 -0400

    layer 0 Web5app  complete - local dwn

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import {Web5 } from "@web5/api";
 import {VerifiableCredential} from "@web5/credentials";
 
-//APP IDENTIFICATION
+//APP IDENTIFICATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 //creating a new Web5 instance which automatically creates a decentralized identifier
+// ---------> the connection function uses "dht" as the DID method
 const {web5, did:maiCodes} =  await Web5.connect();
-console.log(maiCodes); // --> returns DID
 
 //Access Bearer DID is the JSON object which contain metadata about DID ownership
 const {did:maiBearerDid} = await web5.agent.identity.get({didUri: maiCodes});
-console.log(maiBearerDid); // --> returns Bearer DID object
+//console.log(maiCodes); // --> returns DID
+//console.log(maiBearerDid); // --> returns Bearer DID object
 
-//APP VERIFIABLE CREDENTIAL 
+//APP VERIFIABLE CREDENTIAL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const vc = await VerifiableCredential.create({
     type: "Web5QuickstartCompletionCredential",
     issuer: maiCodes,
@@ -22,5 +23,31 @@ const vc = await VerifiableCredential.create({
         expertiseLevel: "Beginner"
     }
 });
+//console.log(`vc: ${vc}`);
 
-console.log(`vc: ${vc}`);
+//ISSUEER SIGNS VERIFIABLE CREDENTIAL (conversion to JSON Web Token)
+const signedVc = await vc.sign({did:maiBearerDid });
+//console.log(signedVc);
+
+//STORING VC/JWT IN DWN
+
+const {record} = await web5.dwn.records.create({
+    data: signedVc,
+    message: {
+        schema: 'Web5QuickstartCompletionCredential',
+        dataFormat: 'application/vc+jwt',
+        //declaring the record to be viewed w/o permissions
+        published: true
+
+    }
+});
+
+//console.log("writeResult", record);
+
+// READING OPTIONAL PERMISSION DATA FROM DWN
+const readSignedVc = await record.data.text();
+//console.log("read Public", readSignedVc);
+
+//PARSE VC/inspecting VC: converting JWT to JSON
+const parsedVc = VerifiableCredential.parseJwt({vcJwt: readSignedVc});
+console.log(parsedVc);
